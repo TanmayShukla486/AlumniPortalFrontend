588ca5551f8f3904d667a3526e8e0da2445fec3c
Tanmay Shukla <tanmay.shukla629@gmail.com>
2024-11-19 00:57:15 +0530

    added toasts and error handling to create postings page

--- src/app/pages/jobs/JobPosting.tsx
+++ src/app/pages/jobs/JobPosting.tsx
@@ -9,7 +9,16 @@ import {
   selectUsername,
 } from "../../../features/user/userSlice"
 import axios from "axios"
-import { Checkbox, Divider, Input, SvgIcon, TextField } from "@mui/material"
+import {
+  Alert,
+  Checkbox,
+  Divider,
+  Input,
+  Skeleton,
+  Snackbar,
+  SvgIcon,
+  TextField,
+} from "@mui/material"
 import { Navigate } from "react-router-dom"
 
 export interface Posting {
@@ -28,7 +37,7 @@ export interface Posting {
 const PostingItem = ({ posting }: { posting: Posting }) => {
   return (
     <div
-      className={`bg-gradient-to-r ${posting.status === "APPROVED" ? "from-green-900 to-green-500" : posting.status === "REJECTED" ? "from-red-900 to-red-500" : "from-content-dark to-content-light"} w-full px-4 py-2 flex flex-row items-center justify-start border-content-light border-2 rounded-xl`}
+      className={`bg-gradient-to-r ${posting.status === "APPROVED" ? "from-green-900 to-green-500" : posting.status === "REJECTED" ? "from-red-900 to-red-500" : "from-content-dark to-content-light"} w-full px-4 py-2 flex flex-row items-center justify-start border-content-dark border-[5px] rounded-xl`}
     >
       {posting.title}
     </div>
@@ -49,6 +58,15 @@ const JobPosting = () => {
   const [sal, setSal] = useState<number>(0)
   const [link, setLink] = useState<string>("")
   const [company, setCompany] = useState<string>("")
+  const [status, setStatus] = useState<"LOADING" | "IDLE" | "ERROR" | "POSTED">(
+    "IDLE",
+  )
+  const [open, setOpen] = useState<boolean>(true)
+  const [postingStatus, setPostingStatus] = useState<
+    "LOADING" | "IDLE" | "ERROR"
+  >("IDLE")
+  const [error, setError] = useState<string>("Error Occurred")
+  const [postingError, setPostingError] = useState<string>("")
   const config: ConfigType = {
     headers: {
       "Content-Type": "application/json",
@@ -59,14 +77,16 @@ const JobPosting = () => {
   }
   const fetchPostings = async () => {
     try {
+      setPostingStatus("LOADING")
       const response = await axios.get(
         `http://localhost:8080/api/posting/username/${username}`,
         config,
       )
       setPostings(response.data)
-      console.log(response.data)
+      setPostingStatus("IDLE")
     } catch (error) {
-      console.log(error)
+      setPostingStatus("ERROR")
+      if (error instanceof Error) setPostingError(error.message)
       setPostings([])
     }
   }
@@ -87,6 +107,7 @@ const JobPosting = () => {
       status: "PENDING",
     }
     try {
+      setStatus("LOADING")
       const response = await axios.post(
         "http://localhost:8080/api/postings",
         JSON.stringify(posting),
@@ -98,13 +119,45 @@ const JobPosting = () => {
       setLoc("")
       setDesc("")
       setLink("")
+      setStatus("POSTED")
     } catch (error) {
-      console.log(error)
+      if (error instanceof Error) setError(error.message)
+      setStatus("ERROR")
     }
   }
 
   return (
     <Wrapper>
+      <div>
+        <Snackbar
+          open={status === "ERROR" && open}
+          autoHideDuration={1500}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        >
+          <Alert
+            severity="error"
+            variant="filled"
+            className="cursor-pointer"
+            onClick={() => setOpen(!open)}
+          >
+            {error}
+          </Alert>
+        </Snackbar>
+        <Snackbar
+          open={status === "POSTED"}
+          autoHideDuration={1500}
+          anchorOrigin={{ vertical: "bottom", horizontal: "center" }}
+        >
+          <Alert
+            severity="success"
+            variant="filled"
+            className="cursor-pointer"
+            onClick={() => setStatus("IDLE")}
+          >
+            Job Posting Successful!
+          </Alert>
+        </Snackbar>
+      </div>
       <div className="grid grid-cols-10 gap-x-4 mt-2 pr-4 h-[83.5vh]">
         <div className="col-span-7 border-2 border-white shadow-custom bg-gradient-to-b from-blue-400/20 to-content-dark rounded-xl pt-2 px-2">
           <div className="w-full h-full">
@@ -334,7 +387,7 @@ const JobPosting = () => {
             </div>
           </div>
         </div>
-        <div className="col-span-3 border-2 border-white shadow-custom bg-content-dark rounded-xl p-4">
+        <div className="col-span-3 border-2 border-white shadow-custom bg-gradient-to-b from-content-light/60 to-white rounded-xl p-4">
           <div className="text-white text-2xl font-bold mb-2">Postings</div>
           <Divider
             sx={{
@@ -342,14 +395,26 @@ const JobPosting = () => {
               opacity: "50%",
             }}
           />
-          {postings.length > 0 && (
+          {postingStatus === "LOADING" && (
+            <div>
+              <Skeleton sx={{ height: "56px" }} />
+              <Skeleton sx={{ height: "56px" }} />
+              <Skeleton sx={{ height: "56px" }} />
+            </div>
+          )}
+          {postingStatus === "ERROR" && (
+            <div className="text-sm text-white opacity-50">
+              Couldn't fetch postings due to error: {postingError}
+            </div>
+          )}
+          {postingStatus === "IDLE" && postings.length > 0 && (
             <div className="text-white h-full py-2">
               <div className="h-1/3">
-                <div>Approved</div>
+                <div className="text-content-dark">Approved</div>
                 <Divider
                   sx={{
                     opacity: "50%",
-                    borderColor: "white",
+                    borderColor: "#052238",
                     width: "33%",
                     marginBottom: "4px",
                   }}
@@ -362,18 +427,18 @@ const JobPosting = () => {
                       .map(it => <PostingItem posting={it} />)}
                   {postings.filter(it => it.status === "APPROVED").length ===
                     0 && (
-                    <div className="text-sm opacity-50">
+                    <div className="text-sm opacity-50 text-content-dark">
                       No approved requests
                     </div>
                   )}
                 </div>
               </div>
               <div className="h-1/3">
-                <div>Pending</div>
+                <div className="text-content-dark">Pending</div>
                 <Divider
                   sx={{
                     opacity: "50%",
-                    borderColor: "white",
+                    borderColor: "#052238",
                     width: "33%",
                     marginBottom: "4px",
                   }}
@@ -385,7 +450,7 @@ const JobPosting = () => {
                       .map(it => <PostingItem posting={it} />)}
                   {postings.filter(it => it.status === "PENDING").length ===
                     0 && (
-                    <div className="text-sm opacity-50">
+                    <div className="text-sm opacity-50 text-content-dark">
                       No approved requests
                     </div>
                   )}
@@ -393,11 +458,11 @@ const JobPosting = () => {
               </div>
 
               <div className="h-1/3">
-                <div>Rejected</div>
+                <div className="text-content-dark">Rejected</div>
                 <Divider
                   sx={{
                     opacity: "50%",
-                    borderColor: "white",
+                    borderColor: "#052238",
                     width: "33%",
                     marginBottom: "4px",
                   }}
@@ -410,7 +475,7 @@ const JobPosting = () => {
                       .map(it => <PostingItem posting={it} />)}
                   {postings.filter(it => it.status === "REJECTED").length ===
                     0 && (
-                    <div className="text-sm opacity-50">
+                    <div className="text-sm opacity-50 text-content-dark">
                       No rejected requests
                     </div>
                   )}
@@ -418,8 +483,10 @@ const JobPosting = () => {
               </div>
             </div>
           )}
-          {postings.length === 0 && (
-            <div className="opacity-50 text-white">No job postings yet</div>
+          {postingStatus === "IDLE" && postings.length === 0 && (
+            <div className="opacity-50 text-content-dark">
+              No job postings yet
+            </div>
           )}
         </div>
       </div>
